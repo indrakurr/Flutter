1b8fcbbac4d972bd7c157454d9d99187cf54472f
Indra Kurniawan <kurniawanindra9d@gmail.com>
2023-08-18 13:34:43 +0700

    add belajar_flutter

---
+++ belajar_flutter/bin/belajar_flutter.dart
@@ -0,0 +1,5 @@
+import 'package:belajar_flutter/belajar_flutter.dart' as belajar_flutter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${belajar_flutter.calculate()}!');
+}
---
+++ belajar_flutter/flutter_application_belajar/android/app/src/main/kotlin/com/example/flutter_application_belajar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_application_belajar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
