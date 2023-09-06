b54f1c4d39bf43280262fabe8a6d2ef94124467d
Indra Kurniawan <kurniawanindra9d@gmail.com>
2023-09-06 07:47:31 +0700

    add latihan_container

---
+++ latihan_container/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple,
+          title: Text("Latihan Container"),
+        ),
+        body: Container(
+          color: Colors.black,
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: <Color>[Colors.white, Colors.deepPurple])),
+          ),
+        ),
+      ),
+    );
+  }
+}
